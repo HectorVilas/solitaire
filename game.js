a6e42b269a02992178bdbe2c32d4e95e77ab24d1
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-05-23 15:24:53 -0300

    shuffling and card lying code

--- game.js
+++ game.js
@@ -3,23 +3,50 @@ let cards = []
 let table = {
   stock: [],
   waste: [],
-  foundations: { pile1: [1], pile2: [2], pile3: [3], pile4: [4] },
-  tableau: {pile1: [], pile2: [], pile3: [], pile4: [],
-    pile5: [], pile6: [], pile7: [] },
+  foundations: [[],[],[],[]],
+  tableau: [[],[],[],[],[],[],[]],
 }
 
-//creating cards
-let suitsList = ["club", "diamonds", "spades", "hearts"]
-
-for(let suitLoop = 0; suitLoop < 4; suitLoop++){
-  for(let cardLoop = 1; cardLoop < 14; cardLoop++){
-    let suit, number, color
-    suit = suitsList[suitLoop]
-    number = cardLoop
-    
-    suitLoop%2 === 0 ? color = "black": color = "red"
-    
-    let card = { suit, number, color, isFlipped: false }
-    cards.push(card)
+function cardCreation(){
+  let suitsList = ["club", "diamonds", "spades", "hearts"]
+  
+  for(let suitLoop = 0; suitLoop < 4; suitLoop++){
+    for(let cardLoop = 1; cardLoop < 14; cardLoop++){
+      let suit, number, color
+      number = cardLoop
+      suit = suitsList[suitLoop]
+      
+      suitLoop%2 === 0 ? color = "black": color = "red"
+      
+      let card = { number, suit, color, isFlipped: false }
+      cards.push(card)
+    }
   }
 }
+
+function shuffleCards(){
+  for (let i = 0; i < 10; i++) {
+    cards.sort(() => Math.random() > 0.5)
+  }
+}
+
+function layCards(){
+  //tableau
+  for(let i = 0; i < table.tableau.length; i++){
+    let quantity = i;
+    for (let j = quantity+1; j > 0; j--) {
+      table.tableau[i].push(cards[0])
+      cards.shift()
+    }
+  }
+  //wastepile
+  table.waste.push(cards[0])
+  cards.shift()
+  //stock
+  table.foundations = cards
+  cards = [];
+}
+
+cardCreation()
+shuffleCards()
+layCards()
