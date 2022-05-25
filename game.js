193e58ad2d3f1284413241984e8daaa696356926
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-05-25 15:25:47 -0300

    renaming and more checks

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-let cardFrom;
-let cardTo;
+let from, to;
+const tabIDs = ["tab-0","tab-1","tab-2","tab-3","tab-4","tab-5","tab-6"]
 
-let cards = []
+let deck = []
 
 let table = {
   stock: [],
@@ -10,9 +10,9 @@ let table = {
   tableau: [[],[],[],[],[],[],[]],
 }
 
-const $stock = document.querySelector(".stock");
-const $wastepile = document.querySelector(".wastepile");
-const $infoSpace = document.querySelector(".info-space");
+const $stock = document.querySelector("#stock");
+const $wastepile = document.querySelector("#wastepile");
+const $infoSpace = document.querySelector("#info-space");
 const $foundations = document.querySelectorAll(".foundation");
 const $tableaus = document.querySelectorAll(".tableau");
 
@@ -29,14 +29,14 @@ function cardCreation(){
       suitLoop%2 === 0 ? color = "black": color = "red"
       
       let card = { number, suit, color, isFlipped: false }
-      cards.push(card)
+      deck.push(card)
     }
   }
 }
 
 function shuffleCards(){
   for (let i = 0; i < 10; i++) {
-    cards.sort(() => Math.random() > 0.5)
+    deck.sort(() => Math.random() > 0.5)
   }
 }
 
@@ -44,14 +44,14 @@ function layCards(){
   for(let i = 0; i < table.tableau.length; i++){
     let quantity = i;
     for (let j = quantity+1; j > 0; j--) {
-      table.tableau[i].push(cards[0])
-      cards.shift()
+      table.tableau[i].push(deck[0])
+      deck.shift()
     }
   }
-  table.waste.push(cards[0])
-  cards.shift()
-  table.stock = cards
-  cards = []
+  table.waste.push(deck[0])
+  deck.shift()
+  table.stock = deck
+  deck = []
 }
 
 //adding divisions in wastepile and tableau
@@ -61,13 +61,11 @@ function domDivisions(){
       let separator = document.createElement("div")
       separator.classList.add("separator",`n${space}`)
       
-      separator.addEventListener("mousedown", (e) => {
-        clickAction("mousedown", pile, space)
-        e.preventDefault()
+      separator.addEventListener("mousedown", () => {
+        clickAction("mousedown", separator.parentNode.id, pile, space)
       })
-      separator.addEventListener("mouseup", (e) => {
-        clickAction("mouseup", pile,space)
-        e.preventDefault()
+      separator.addEventListener("mouseup", () => {
+        clickAction("mouseup", separator.parentNode.id, pile, space)
       })
 
       thisTableau.appendChild(separator)
@@ -75,45 +73,51 @@ function domDivisions(){
   })
 }
 //action to store the interacting cards
-function clickAction(action, pile, space){
+function clickAction(action, place, pile, space){
+  let cardValue = {place,pile,space}
   if(action === "mousedown"){
-    cardFrom = {pile: pile, space: space}
+    from = cardValue
   } else if(action === "mouseup"){
-    cardTo = {pile: pile, space: space}
+    to = cardValue
     isValidMove()
-    //removing values to helper variables
-    cardFrom = undefined
-    cardTo = undefined
+    // removing values to helper variables
+    from = undefined
+    to = undefined
   }
 }
 //check if card can be moved to another pile
 function isValidMove(){
-  let from = table.tableau[cardFrom.pile][cardFrom.space]
-  let to = table.tableau[cardTo.pile][cardTo.space]
-  //do nothing if origin or destination is an empty space
-  if(from == undefined || to == undefined) return
-  //check if card can be placed
-  if(from.number === to.number-1
-    && cardFrom.pile !== cardTo.pile
+  //prevent errors when dragging from invalid spaces
+  if(from === undefined || to === undefined) return;
+  //storing the card info
+  let cardFrom = table.tableau[from.pile][from.space]
+  let cardTo = table.tableau[to.pile][to.space]
+
+  if(cardFrom !== undefined && cardTo !== undefined //spaces have cards
+    &&cardFrom.number === cardTo.number-1 //valid numbers
+    && from.pile !== to.pile //different piles
     // && to.isFlipped === true //disabled temporally
-    //check if the destination is the last card in the pile
-    && table.tableau[cardTo.pile][cardTo.space]
-    === table.tableau[cardTo.pile][table.tableau[cardTo.pile].length-1]
+    && table.tableau[to.pile][to.space] //destination is the last card -
+    === table.tableau[to.pile][table.tableau[to.pile].length-1] // - of pile
     ){
     console.log("valid move");
+    if(tabIDs.includes(from.place) && tabIDs.includes(to.place)){
+      console.log("tableau pile to tableau pile");
+    }
+
   } else {
     console.log("NOT valid move");
   }
 }
 //placing cards on each tableau pile's space
 function placeCardsDom(){
-  table.stock.forEach(card => $stock.innerText += ` ${card.suit} ${card.number}`)
+  // table.stock.forEach(card => $stock.innerText += ` ${card.suit} ${card.number}`)
 
-  $wastepile.innerText = ` ${table.waste[0].suit} ${table.waste[0].number}`
+  // $wastepile.innerText = ` ${table.waste[0].suit} ${table.waste[0].number}`
 
   for(let i = 0; i < table.tableau.length; i++){
     for (let j = 0; j < table.tableau[i].length; j++) {
-      let space = document.querySelector(`.tab-${i} .n${j}`)
+      let space = document.querySelector(`#tab-${i} .n${j}`)
       space.innerText = `${table.tableau[i][j].suit} ${table.tableau[i][j].number}`
     }
   }
